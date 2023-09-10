0310409ec6ed787a8a2f40c5f8bde07eadbc4d1a
gauna <iamgauna@gmail.com>
2023-09-09 23:05:48 -0400

    Validacion del tipo de dato Email

--- src/main/java/com/somosetica/domain/Persona.java
+++ src/main/java/com/somosetica/domain/Persona.java
@@ -2,6 +2,7 @@ package com.somosetica.domain;
 
 import java.io.Serializable;
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import lombok.Data;
 
@@ -26,6 +27,7 @@ public class Persona implements Serializable{
     private String contrasena;
     
     @NotEmpty
+    @Email
     private String email;
     
     @NotEmpty
