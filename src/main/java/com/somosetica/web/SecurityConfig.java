ff9ed7cf116b8ece3497d0659dffdd029cb3804a
gauna <iamgauna@gmail.com>
2023-09-09 23:57:05 -0400

    Agregamos la configuracion para un usuario en memoria

--- src/main/java/com/somosetica/web/SecurityConfig.java
+++ src/main/java/com/somosetica/web/SecurityConfig.java
@@ -1,6 +1,19 @@
 package com.somosetica.web;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-public class SecurityConfig {
+@Configuration //Indicamos que es una clase de configuracion
+@EnableWebSecurity //Habilitamos la seguridad WEB
+public class SecurityConfig extends WebSecurityConfigurerAdapter{//WebSecurityConfigurerAdapter utilizaremos para configurar los usuarios que vamos a utilizar
     
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("eko")
+                    .password("{noop}etica")
+                    .roles("ADMIN","USER");
+    }
 }
