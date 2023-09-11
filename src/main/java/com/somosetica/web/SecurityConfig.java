3268fbfe0dfc93541bc139e2675dc5af7382e9b7
gauna <iamgauna@gmail.com>
2023-09-11 13:01:24 -0400

    configuraciones para el encriptamiento

--- src/main/java/com/somosetica/web/SecurityConfig.java
+++ src/main/java/com/somosetica/web/SecurityConfig.java
@@ -5,10 +5,30 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration //Indicamos que es una clase de configuracion
 @EnableWebSecurity //Habilitamos la seguridad WEB
 public class SecurityConfig extends WebSecurityConfigurerAdapter{//WebSecurityConfigurerAdapter utilizaremos para configurar los usuarios que vamos a utilizar
+    
+    /*/Manejo de los usuarios desde la tablas rol y Usuarios:
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Autowired
+    public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception{
+        build.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }*/
+    
+    
     
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
